4ee99b72a5d64ad9c473d7ef0fb7b5057991fe96
derGreen <dergreen@gmail.com>
2017-10-13 11:02:48 +0200

    update to new version
    this main.js habve some Bugs with the 'Speechout' in the StreamIntent function

--- main.js
+++ main.js
@@ -1,90 +1,90 @@
 "use strict";
 
-/*/
+//
 //https://api.twitch.tv/kraken/channels/therealhellcat?client_id=xiujxr8qj65oeu2nwmyjs4ltpb6kqm
-/*/
+//
 
 const Alexa = require('alexa-sdk');
 var https = require('https');
-var channelID = 'gronkh', clientID = 'xiujxr8qj65oeu2nwmyjs4ltpb6kqm';
+var clientID = 'xiujxr8qj65oeu2nwmyjs4ltpb6kqm';
+
+Alexa.appId = 'amzn1.ask.skill.b20c6a98-8182-4a3a-bb8f-285170d61e0a';
 
 var handlers = {
+    "HalloIntent": function() {
+         var name = this.event.request.intent.slots.Name.value;
 
-    "HellCatIntent": function () {
-
-        var responseString = '', mythis = this, HellcatID = 'therealhellcat';
-
-        https.get('https://api.twitch.tv/kraken/streams/' + HellcatID + '?client_id=' + clientID, (res) => {
-
-            res.on('data', (d) => {
-                responseString += d;
-            });
-
-            res.on('end', function (res) {
-                const speechOutput = responseString;
-                var json = JSON.parse(speechOutput);
-                if (json.stream === null) {
-                    mythis.emit(':tell', 'leider streamt er gerade nicht');
-                } else {
-                    mythis.emit(':tell', 'Ja, er ist live und streamt ' + json.stream.game + '. Mit dem Motto ' + json.stream.channel.status);
-                }
-
-            });
-
-        }).on('error', (e) => {
-            console.error(e);
-        });
+         this.emit(':tell', 'Hi ' + name  );
 
     },
-    "NowIntent": function () {
 
-        var responseString = '', mythis = this;
+    "StreamIntent": function () {
 
-        https.get('https://api.twitch.tv/kraken/streams/' + channelID + '?client_id=' + clientID, (res) => {
+       console.log('---- Debug Log ----  Stream Intent läuft');
 
-            res.on('data', (d) => {
-                responseString += d;
-            });
+    var responseString = '',
+    speechOutput= '',
+    datas,
+    reprompt = 'noch jemand ?',
+    mythis = this,
+    channelID = this.event.request.intent.slots.StreamID.resolutions.resolutionsPerAuthority[0].values[0].value.id;
 
-            res.on('end', function (res) {
-                const speechOutput = responseString;
-                var json = JSON.parse(speechOutput);
-                if (json.stream === null) {
-                    mythis.emit(':tell', channelID + ' ist Offline');
-                } else {
-                    mythis.emit(':tell', channelID + ' streamt ' + json.stream.game + '. Heute mit dem Titel: ' + json.stream.channel.status);
-                }
+    console.log('---- Debug Log ---- suche läuft nach ' + channelID);
 
-            });
+    if (channelID === undefined) {
+        speechOutput = 'Sorry, Ich kenne diesen Streamer noch nicht';
+    }
 
-        }).on('error', (e) => {
-            console.error(e);
-        });
+    https.get('https://api.twitch.tv/kraken/streams/' + channelID + '?client_id=' + clientID , (res) => {
 
-    },
+       console.log('---- Debug Log ----Api call an '+'https://api.twitch.tv/kraken/streams/' + channelID + '?client_id=' + clientID);
+
+      res.on('data', (d) => {
+        datas += d;
+        if (d === undefined){
+            speechOutput = 'ich kann keine Daten von ' + channelID + 'bekommen';
+        }
+      });
+
+      res.on('end', function(res) {
+        const object = datas;
+        var json = JSON.parse(object);
+       if (json.stream === null) {
+          speechOutput = channelID + ' ist momentan Offline';
+       }else {
+           speechOutput = channelID + ' streamt ' + json.stream.game + '. Heute mit dem Titel: ' + json.stream.channel.status;
+       }
+
+      });
+
+    }).on('error', (e) => {
+      console.error(e);
+    });
+    this.emit(':ask', speechOutput, reprompt);
+  },
     "LaunchRequest": function () {
-        var speechOutput = "Stream Agent im dienst! Ich überwache aktuell " + channelID + " und Hell Cat";
-        var reprompt = "was soll ich tun?";
-        this.emit(':ask', speechOutput, reprompt);
-    },
+    var speechOutput = "Stream Agent gestartet";
+    var reprompt = "frag mich zu einem Streamer";
+    this.emit(':ask', speechOutput, reprompt);
+  },
 
     "AMAZON.HelpIntent": function () {
-        var speechOutput = "Ich sag dir ob Hell Cat live ist, frag einfach: ist Helcat online ? ";
-        var reprompt = "was soll ich tun?";
-        this.emit(':ask', speechOutput, reprompt);
+    var speechOutput = "Hallo, Ich sag dir ob Streamer live auf Twitch sind, frage zum Beispiel, ist therealHellcat live ? ";
+    var reprompt = "was soll ich tun?";
+    this.emit(':ask', speechOutput, reprompt);
     },
     "AMAZON.StopIntent": function () {
         var speechOutput = "OK";
-        this.emit(':tell', speechOutput);
+    this.emit(':tell', speechOutput );
     },
     "AMAZON.CancelIntent": function () {
-        var speechOutput = "Alle Maschinen stop!";
-        this.emit(':tell', speechOutput);
-    }
+        var speechOutput = "auf wieder hören";
+    this.emit(':tell', speechOutput );
+   }
 };
 
-exports.handler = function (event, context, callback) {
-    var alexa = Alexa.handler(event, context);
+exports.handler = function(event, context, callback){
+  var alexa = Alexa.handler(event, context);
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
